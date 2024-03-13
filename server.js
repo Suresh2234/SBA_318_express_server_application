11f73ad6e407526aafbb3bd7465639dd362231d6
Suresh Patil <patilrsuresh@outlook.com>
2024-03-13 19:25:44 -0400

    commit 5

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Set the view engine to use EJS
+app.set('view engine', 'ejs');
+
 // Middleware
 app.use(express.static('public'));
 
